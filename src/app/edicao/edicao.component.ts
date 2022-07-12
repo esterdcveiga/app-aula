50dac95ccbca0df0d2ca2083a3eee39d976e5f94
Ester Veiga <esterdcveiga@gmail.com>
2022-07-12 13:25:56 -0300

    alterando visibilidade de um bloco

--- src/app/edicao/edicao.component.ts
+++ src/app/edicao/edicao.component.ts
@@ -6,10 +6,24 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./edicao.component.css']
 })
 export class EdicaoComponent implements OnInit {
+  blocoVisivel: boolean = false
+  nomeBotao:string = 'show'
 
   constructor() { }
 
   ngOnInit(): void {
   }
+
+  show():void{
+    this.blocoVisivel= !this.blocoVisivel
+    if (this.blocoVisivel == false) {
+      //this.blocoVisivel = true
+      this.nomeBotao='hide'
+    } else {
+      //this.blocoVisivel = false
+      this.nomeBotao='show'     
+    }
+  }
+  
 
 }
