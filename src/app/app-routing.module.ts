769b4ab15a6fc9234588747782ec39e90f576467
Ester Veiga <esterdcveiga@gmail.com>
2022-07-12 08:51:41 -0300

    adicionando navegação

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,25 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+//importando classes para criação das rotas
+import { CadastroComponent } from "./cadastro/cadastro.component";
+import { ConsultaComponent } from "./consulta/consulta.component";
+import { EdicaoComponent } from "./edicao/edicao.component";
 
-const routes: Routes = [];
+//crinando as rotas para navegação
+const routes: Routes = [
+  {
+    path: 'cadastro',
+    component: CadastroComponent
+  },
+  {
+    path: 'consulta',
+    component: ConsultaComponent
+  },
+  {
+    path: 'edicao',
+    component: EdicaoComponent
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
