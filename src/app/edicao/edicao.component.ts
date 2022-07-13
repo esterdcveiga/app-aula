5d0c6f650fa1b04a2088b704dc793503047d92a9
Ester Veiga <esterdcveiga@gmail.com>
2022-07-12 22:51:19 -0300

    observações

--- src/app/edicao/edicao.component.ts
+++ src/app/edicao/edicao.component.ts
@@ -14,6 +14,7 @@ export class EdicaoComponent implements OnInit {
   ngOnInit(): void {
   }
 
+  //mótodo que altera valor do atributo bloco sempre que o botão é cllicado e altera também o texto referente ao nome do botão de acordo com o valor do atributo blocoVisivel
   show():void{
     this.blocoVisivel= !this.blocoVisivel
     
